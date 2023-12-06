67206d08c6d14b5e7f145ac7c67d968cb5b1f267
Kevin Hintz <khintz34@gmail.com>
2023-12-06 15:33:24 -0600

    hero

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,40 @@
+"use client";
+import HeroDesktop from "../../assets/images/HeroDesktop.jpg";
+import Image from "next/image";
+import styles from "./Hero.module.scss";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IconContext } from "react-icons";
+import { useState } from "react";
+
+export function Hero() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.header}>
+        <div className={styles.logo}>
+          <h1 className={styles.logoH1}>Sam Pausha</h1>
+          <h2 className={styles.logoH2}>Fitness Coaching</h2>
+        </div>
+        <div className={styles.hamburgerIcon}>
+          <IconContext.Provider value={{ size: "40px" }}>
+            <GiHamburgerMenu />
+          </IconContext.Provider>
+        </div>
+      </div>
+      <div className={styles.imageWrapper}>
+        <Image
+          width={1440}
+          src={HeroDesktop}
+          height={700}
+          alt="Sam Pausha riding his bike in the Madison Ironman"
+          className={styles.heroImage}
+        />
+        <div className={styles.overlay}></div>
+      </div>
+      <div className={styles.heroFooter}>
+        <div className={`${styles.red} ${styles.heroType}`}>SWIM</div>
+        <div className={`${styles.heroType}`}>BIKE</div>
+        <div className={`${styles.blue} ${styles.heroType}`}>RUN</div>
+      </div>
+    </main>
+  );
+}
