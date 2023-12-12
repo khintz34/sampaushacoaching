b3968d608d6a408b7a656323305b148de8b3f151
Kevin Hintz <khintz34@gmail.com>
2023-12-12 16:59:18 -0600

    hero changes

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,29 +6,22 @@ import styles from "./Hero.module.scss";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IconContext } from "react-icons";
 import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
+
+// todo create comp for Logo
+// todo media query for ipad
+// todo media query for galazyFold
 
 export function Hero() {
   const ICON_SIZE = "40px";
-  const [mobile, setMobile] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (screen.width > 600) {
-      setMobile(false);
-    } else {
-      setMobile(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    console.log(mobile);
-  }, [mobile]);
+  const isMobile = useMediaQuery({ maxWidth: 768 });
 
   return (
     <main className={styles.main}>
       <div className={styles.header}>
         <div className={styles.logo}>
-          <h1 className={styles.logoH1}>Sam Pausha</h1>
-          <h2 className={styles.logoH2}>Fitness Coaching</h2>
+          <h1 className={styles.logoName}>Sam Pausha</h1>
+          <h2 className={styles.logoLabel}>Fitness Coaching</h2>
         </div>
         <div className={styles.hamburgerIcon}>
           <IconContext.Provider value={{ size: ICON_SIZE }}>
@@ -37,14 +30,15 @@ export function Hero() {
         </div>
       </div>
       <div className={styles.imageWrapper}>
-        {mobile ? (
+        <div className={`${styles.mobileImage} ${styles.image}`}>
           <Image
             src={MobileThree}
             objectFit={"contain"}
             alt="Sam Pausha running in the Madison Ironman"
             className={styles.heroImage}
           />
-        ) : (
+        </div>
+        <div className={`${styles.desktopImage} ${styles.image}`}>
           <Image
             width={1440}
             src={HeroDesktop}
@@ -52,14 +46,16 @@ export function Hero() {
             alt="Sam Pausha riding his bike in the Madison Ironman"
             className={styles.heroImage}
           />
-        )}
+        </div>
 
         <div className={styles.overlay} />
       </div>
       <div className={styles.heroFooter}>
-        <div className={`${styles.red} ${styles.heroType}`}>SWIM</div>
-        <div className={`${styles.white} ${styles.heroType}`}>BIKE</div>
-        <div className={`${styles.blue} ${styles.heroType}`}>RUN</div>
+        <div className={`${styles.red} ${styles.exerciseTypeTitle}`}>SWIM</div>
+        <div className={`${styles.white} ${styles.exerciseTypeTitle}`}>
+          BIKE
+        </div>
+        <div className={`${styles.blue} ${styles.exerciseTypeTitle}`}>RUN</div>
       </div>
     </main>
   );
