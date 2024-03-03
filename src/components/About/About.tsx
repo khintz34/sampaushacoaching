7f91c4a9de5bd1645436b5df96614ca641a6cf60
Kevin Hintz <khintz34@gmail.com>
2024-03-02 21:09:19 -0600

    sams changes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,8 +6,8 @@ import classNames from "classnames";
 import ArmsUpImageSrc from "../../assets/images/ArmsUp.jpg";
 import { Qualifications } from "../Qualifications/Qualifications";
 import {
-  QUALIFICATIONS_TEXT,
   ABOUT_BIO,
+  QUALIFICATIONS_DATA,
 } from "../../assets/data/QualificationsData";
 
 export function About() {
@@ -38,10 +38,26 @@ export function About() {
           <h3 className={styles.h3New}>Triathalon Coaching</h3>
           <h2 className={styles.h2}>Sam Pausha</h2>
         </div>
-        <p className={styles.aboutParagraph}>{ABOUT_BIO}</p>
+        <p className={styles.aboutParagraph}>
+          {ABOUT_BIO.split("\n").map((line, index) => {
+            return (
+              <span key={index}>
+                {line}
+                <br />
+              </span>
+            );
+          })}
+        </p>
         <div className={styles.qualificationsContainer}>
-          {QUALIFICATIONS_TEXT.map((qualification, index) => {
-            return <Qualifications Qualification={qualification} key={index} />;
+          {QUALIFICATIONS_DATA.map((qualification, index) => {
+            return (
+              <Qualifications
+                Qualification={qualification.title}
+                imageSrc={qualification.imageSrc}
+                imageAlt={qualification.imageAlt}
+                key={index}
+              />
+            );
           })}
         </div>
       </div>
--- src/components/Qualifications/Qualifications.tsx
+++ src/components/Qualifications/Qualifications.tsx
@@ -1,9 +1,29 @@
+import Image from "next/image";
 import styles from "./Qualifications.module.scss";
 
 type Props = {
   Qualification: string;
+  imageSrc: string;
+  imageAlt: string;
 };
 
-export function Qualifications({ Qualification }: Props) {
-  return <div className={styles.QualificationsMain}>{Qualification}</div>;
+export function Qualifications({ Qualification, imageSrc, imageAlt }: Props) {
+  const imageStyle = {
+    borderRadius: "50%",
+  };
+
+  return (
+    <div className={styles.QualificationsMain}>
+      <div className={styles.imageContainer}>
+        <Image
+          src={imageSrc}
+          style={imageStyle}
+          height={20}
+          width={20}
+          alt={imageAlt}
+        />
+      </div>
+      {Qualification}
+    </div>
+  );
 }
